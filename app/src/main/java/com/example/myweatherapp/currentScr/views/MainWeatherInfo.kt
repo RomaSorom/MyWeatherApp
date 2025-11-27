ab171d8d6940f54e0956c8eefed60a610f81ee21
RomaSorom <romasoromm@gmail.com>
2025-11-27 17:55:25 +0200

    change img size

--- app/src/main/java/com/example/myweatherapp/currentScr/views/MainWeatherInfo.kt
+++ app/src/main/java/com/example/myweatherapp/currentScr/views/MainWeatherInfo.kt
@@ -6,6 +6,7 @@ import com.example.myweatherapp.R
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -15,7 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.myweatherapp.forecastScr.views.Placeholder
+import com.example.myweatherapp.sharedViews.Placeholder
 import com.example.myweatherapp.ui.theme.MyWeatherAppTheme
 
 @Composable
@@ -26,11 +27,13 @@ fun MainWeatherInfo(@DrawableRes weatherTypeIcon: Int?,
     Column(modifier = modifier,
            horizontalAlignment = Alignment.CenterHorizontally) {
         if (weatherTypeIcon == null) {
-            Placeholder(modifier = Modifier.width(width = 57.dp)
-                        .height(height = 48.dp))
+            Placeholder(modifier = Modifier.width(width = 150.dp)
+                        .height(height = 125.dp))
         } else {
             Image(painter = painterResource(id = weatherTypeIcon),
-                  contentDescription = null)
+                  contentDescription = null,
+                  modifier = Modifier.size(width = 150.dp,
+                                           height = 125.dp))
         }
         Spacer(modifier = Modifier.height(height = 20.dp))
         if (temp == null) {
