f5a8bb282044b40cc52f2d4d3bc40fcc36ff1e7b
RomaSorom <romasoromm@gmail.com>
2025-11-27 16:56:01 +0200

    add placeholder in weather parameter

--- app/src/main/java/com/example/myweatherapp/currentScr/views/WeatherParameter.kt
+++ app/src/main/java/com/example/myweatherapp/currentScr/views/WeatherParameter.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -13,11 +14,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.myweatherapp.forecastScr.views.Placeholder
 import com.example.myweatherapp.ui.theme.MyWeatherAppTheme
 
 @Composable
 fun WeatherParameter(parameterName: String,
-                     parameterValue: String,
+                     parameterValue: String?,
                      modifier: Modifier = Modifier,
                      parameterUnit: String? = null) {
     Column(horizontalAlignment = Alignment.CenterHorizontally,
@@ -25,9 +27,14 @@ fun WeatherParameter(parameterName: String,
         Text(text = parameterName,
              style = MaterialTheme.typography.bodySmall)
         Spacer(modifier = Modifier.height(height = 5.dp))
-        Text(text = if (parameterUnit != null) "$parameterValue $parameterUnit" else parameterValue,
-             style = MaterialTheme.typography.bodyLarge,
-             fontWeight = FontWeight.W500)
+        if (parameterValue != null) {
+            Text(text = if (parameterUnit != null) "$parameterValue $parameterUnit" else parameterValue,
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.W500)
+        } else {
+            Placeholder(modifier = Modifier.width(width = 35.dp)
+                        .height(height = 18.dp))
+        }
     }
 }
 
@@ -36,7 +43,7 @@ fun WeatherParameter(parameterName: String,
 fun WeatherParameterPreview() {
     MyWeatherAppTheme {
         Surface {
-            WeatherParameter(parameterValue = "1036",
+            WeatherParameter(parameterValue = null,
                              parameterName = "Pressure",
                              parameterUnit = "hPa",
                              modifier = Modifier.padding(20.dp))
