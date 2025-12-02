980d28d51057b8971abef5968ddc67942536c355
RomaSorom <romasoromm@gmail.com>
2025-12-02 14:24:27 +0200

    add placeholder anim colors constants

---
+++ app/src/main/java/com/example/myweatherapp/sharedViews/PlaceholderConstants.kt
@@ -0,0 +1,8 @@
+package com.example.myweatherapp.sharedViews
+
+import androidx.compose.ui.graphics.Color
+
+object AnimColors {
+    const val INITIAL = 0xFFC0C0C0
+    const val TARGET = 0xFF505050
+}
